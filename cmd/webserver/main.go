120dcd1158459d677cfe9f60c5ac9e025c188e48
Thomas Chobert <tchobert@f4r4s15.clusters.42paris.fr>
2026-01-28 18:58:46 +0100

    including two contexts in main

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -5,23 +5,35 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/TChobert/Go_HTTPS_server/internal/router"
 	"github.com/TChobert/Go_HTTPS_server/internal/server"
 )
 
 func main() {
 
-	rout := router.NewRouter()
-	srv := server.NewServer(rout)
-	c := make(chan os.Signal, 1)
+	r := router.NewRouter()
+	srv := server.NewServer(r)
+	signalCtx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM
+	)
+	defer stop() // this function is defered -> it will be called when main returns
 
-	signal.Notify(c, os.Interrupt)
 	certPath := os.Getenv("TLS_CERT_PATH")
 	keyPath := os.Getenv("TLS_KEY_PATH")
 	go func() {
 		if err := server.RunTLS(srv, certPath, keyPath); err != nil {
 			log.Fatal(err)
 		}
-	} ()
-	select {}
+	}()
+	<-signalCtx.Done()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	server.Shutdown(shutdownCtx, srv)
 }
